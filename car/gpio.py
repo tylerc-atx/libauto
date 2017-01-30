44c75f87a88e3ddbc0de3abff854fffd67abbd17
Ryan Henning <ryan@rhobota.com>
2017-01-29 19:16:22 -0600

    Add more gpio stuff

--- car/gpio.py
+++ car/gpio.py
@@ -21,11 +21,35 @@ PWM_RANGE = 100
 
 
 def setup_pwm_on_pin(pin_index):
-    wpi.pinMode(pin_index, 1)  # 0=input, 1=output 2=PWM
+    wpi.pinMode(pin_index, 1)  # 0=input, 1=output, 2=hardPWM
     wpi.softPwmCreate(pin_index, 0, PWM_RANGE)
 
 
 def set_pin_pwm_value(pin_index, value):
     value = int(value / 100.0 * PWM_RANGE)
     wpi.softPwmWrite(pin_index, value)
+
+
+def setup_output_on_pin(pin_index):
+    wpi.pinMode(pin_index, 1)  # 0=input, 1=output, 2=hardPWM
+
+
+def set_output_pin_value(pin_index value):
+    wpi.digitalWrite(pin_index, 1 if value else 0) # 0=low, 1=high
+
+
+def setup_input_on_pin(pin_index):
+    wpi.pinMode(pin_index, 0)  # 0=input, 1=output, 2=hardPWM
+
+
+def query_input_pin(pin_index):
+    return wpi.digitalRead(ECHO) == 1
+
+
+def delay_micros(num_microseconds):
+    wpi.delayMicroseconds(num_microseconds)
+
+
+def query_micros():
+    return wpi.micros()
 
