675a912333f27367589eae2829630c593e9c06f9
Ryan Henning <ryan@rhobota.com>
2017-01-29 19:28:36 -0600

    Finish (?) sonar piece

--- car/sonar.py
+++ car/sonar.py
@@ -1,38 +1,38 @@
-import wiringpi as wpi
-import time
-import os
+from car import STORE
+from car.gpio import (setup_output_on_pin,
+                      setup_input_on_pin,
+                      set_output_pin_value,
+                      query_input_pin,
+                      delay_micros,
+                      query_micros)
 
 
-TRIG = 27
-ECHO = 17
+SONAR_TRIGGER_PIN = STORE.get('SONAR_TRIGGER_PIN', 17)
+SONAR_ECHO_PIN    = STORE.get('SONAR_ECHO_PIN',    27)
 
 
-os.environ['WIRINGPI_GPIOMEM'] = "1"
-import wiringpi as wpi
-wpi.wiringPiSetupGpio()
+setup_output_on_pin(SONAR_TRIGGER_PIN)
+setup_input_on_pin(SONAR_ECHO_PIN)
 
-wpi.pinMode(TRIG, 1)  # 0=input, 1=output 2=PWM
-wpi.pinMode(ECHO, 0)  # 0=input, 1=output 2=PWM
-
-wpi.digitalWrite(TRIG, 0) # 0=low, 1=high
-wpi.delayMicroseconds(100000)  # 0.1 seconds
+set_output_pin_value(SONAR_TRIGGER_PIN, False)
+delay_micros(100000)  # 0.1 seconds
 
 
 def get_echo_micros():
 
-    wpi.digitalWrite(TRIG, 1) # 0=low, 1=high
-    wpi.delayMicroseconds(10)
-    wpi.digitalWrite(TRIG, 0) # 0=low, 1=high
+    set_output_pin_value(SONAR_TRIGGER_PIN, True)
+    delay_micros(10)
+    set_output_pin_value(SONAR_TRIGGER_PIN, False)
 
-    while wpi.digitalRead(ECHO) == 0:
+    while query_input_pin(SONAR_ECHO_PIN) == False:
         pass
 
-    start = wpi.micros()
+    start = query_micros()
 
-    while wpi.digitalRead(ECHO) == 1:
+    while query_input_pin(SONAR_ECHO_PIN) == True:
         pass
 
-    end = wpi.micros()
+    end = query_micros()
 
     return end - start
 
@@ -49,9 +49,11 @@ def compute_distance_feet(echo_micros):
     return distance_feet
 
 
-while True:
-    echo_micros = get_echo_micros()
-    print("meters = {}".format(compute_distance_meters(echo_micros)))
-    print("feet   = {}\n".format(compute_distance_feet(echo_micros)))
-    time.sleep(0.1)
+if __name__ == '__main__':
+    for i in range(100):
+        import time
+        echo_micros = get_echo_micros()
+        print("meters = {}".format(compute_distance_meters(echo_micros)))
+        print("feet   = {}\n".format(compute_distance_feet(echo_micros)))
+        time.sleep(0.1)
 
