a0727730c7aef1ffaa9fd7a962f8f93d9448f4df
Ryan Henning <ryan@rhobota.com>
2017-02-12 19:52:03 +0000

    Add docstrings

--- car/setup.py
+++ car/setup.py
@@ -4,12 +4,18 @@ from car import sonar
 
 
 def set_pins():
+    """
+    Set all the GPIO pins for all the components on the vehicle.
+    """
     throttle._set_throttle_pin()
     steering._set_steering_pin()
     sonar._set_sonar_pins()
 
 
 def calibrate():
+    """
+    Calibrate all the necessary components on the vehicle.
+    """
     throttle._calibrate_esc()
     steering._calibrate_steering()
 
